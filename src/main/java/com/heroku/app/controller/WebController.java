dfd71735c4a62b6ade3786e3b9bfa42c6346df17
jaewon0913 <jaewon9937@gmail.com>
2022-07-17 17:13:29 +0900

    host 수정

--- src/main/java/com/heroku/app/controller/WebController.java
+++ src/main/java/com/heroku/app/controller/WebController.java
@@ -1,14 +1,17 @@
 package com.heroku.app.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class WebController {
 
     @GetMapping("/vue")
     public String vue(){
-
+        log.info("Vue Page");
+        
         return "vue/index";
     }
 }
